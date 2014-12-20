7eab82eb115150f06e41278175970e6c683f5a10
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-21 00:36:07 +0100

    Dynamically choose drawing function depending on grid size.

--- platform_sdcc/platform_view.c
+++ platform_sdcc/platform_view.c
@@ -8,9 +8,9 @@
 #define draw_one_line_of_four_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
 
 const uint8_t state2byte[CF_STATECOUNT] = { 0x0C, 0xCC, 0x30, 0xF0, 0x3C, 0xFC };
+typedef void cell_draw_function( uint8_t row, uint8_t col, cf_cellState_t const state );
 
 void cf_cell_draw_12( uint8_t row, uint8_t col, cf_cellState_t const state )
-//void cf_cell_draw( cf_coordinates_t *const coords, cf_cellState_t const  state )
 {
         uint8_t value = state2byte[state];
         {
@@ -60,6 +60,17 @@ void cf_grid_draw( const cf_grid_t *const grid )
 {
         sint8_t row = grid->dimensions.row;
 
+        cell_draw_function *cdf;
+
+        if ( ( grid->dimensions.row > 12 ) || ( grid->dimensions.col > 12 ) )
+        {
+                cdf = cf_cell_draw_24;
+        }
+        else
+        {
+                cdf = cf_cell_draw_12;
+        }
+
         while ( --row >= 0 )
         {
                 sint8_t col = grid->dimensions.col;
@@ -73,7 +84,7 @@ void cf_grid_draw( const cf_grid_t *const grid )
                                 printf( "r=%d c=%d col=%d ", row, col, state );
                         }
 
-                        cf_cell_draw_12( row, col, state );
+                        cdf( row, col, state );
                 }
         }
 }
