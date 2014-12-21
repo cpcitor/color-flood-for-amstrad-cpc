72a5e5b99d6d1d843f82febac3bbc9875c1aae69
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-21 11:21:33 +0100

    Move grid 16 bytes to the right to center it.

--- platform_sdcc/platform_view.c
+++ platform_sdcc/platform_view.c
@@ -6,7 +6,7 @@
 #define draw_one_byte() screen_write(pos_local, value); ++pos_local
 #define draw_one_line_of_two_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
 #define draw_one_line_of_four_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
-#define grid_origin(row, col, bytes_per_cell_width, chars_height_per_cell_height) screen + row * char_stride * chars_height_per_cell_height + col * bytes_per_cell_width
+#define grid_origin(row, col, bytes_per_cell_width, chars_height_per_cell_height) screen + row * char_stride * chars_height_per_cell_height + col * bytes_per_cell_width + 16
 
 const uint8_t state2byte[CF_STATECOUNT] = { 0x0C, 0xCC, 0x30, 0xF0, 0x3C, 0xFC };
 typedef void cell_draw_function( uint8_t row, uint8_t col, cf_cellState_t const state );
