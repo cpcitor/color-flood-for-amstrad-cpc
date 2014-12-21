1ab189eb2318f1507466c0c1e498470e784bc297
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-21 11:45:21 +0100

    Make more explicit dependency of safety net on screen adress 0xC000.

--- platform_sdcc/screen.h
+++ platform_sdcc/screen.h
@@ -7,6 +7,8 @@ static uint8_t *const screen = 0xC000;
 static const uint8_t char_stride = 80;
 static const uint16_t line_stride = 0x800;
 
+/* WARNING: Only works for screen at 0xC000, other cases would need two tests. */
 #define screen_write(ptr, value) do { assert(((unsigned int)ptr)>=0xc000); *ptr = value ; } while (0)
+/* #define screen_write(ptr, value) do { assert(((unsigned int)ptr)>=((uint16_t)screen)); *ptr = value ; } while (0) */
 
 #endif /* __SCREEN_H__ */
