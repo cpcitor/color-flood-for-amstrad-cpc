efee3240aa835fa3cc6d214a20ae45698f6718c1
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-21 19:51:45 +0100

    Move draw_one_line_of_two_bytes definition closer to its use.

--- platform_sdcc/platform_view.c
+++ platform_sdcc/platform_view.c
@@ -4,7 +4,6 @@
 #include "stdio.h"
 
 #define draw_one_byte() screen_write(pos_local, value); ++pos_local
-#define draw_one_line_of_two_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
 #define grid_origin(row, col, bytes_per_cell_width, chars_height_per_cell_height) screen + row * char_stride * chars_height_per_cell_height + col * bytes_per_cell_width + 16
 
 const uint8_t state2byte[CF_STATECOUNT] = { 0x0C, 0xCC, 0x30, 0xF0, 0x3C, 0xFC };
@@ -46,6 +45,7 @@ void cf_cell_draw_24( uint8_t row, uint8_t col, cf_cellState_t const state )
                 static const int bytes_per_cell_width = 2;
                 uint8_t *pos_this_sel = grid_origin( row, col, bytes_per_cell_width, 1 );
                 uint8_t *pos_local;
+#define draw_one_line_of_two_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
                 draw_one_line_of_two_bytes();
                 draw_one_line_of_two_bytes();
                 draw_one_line_of_two_bytes();
