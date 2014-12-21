6363a24c0281dc641dd72786e86d303ff035a660
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-21 11:18:40 +0100

    Factor grid position.

--- platform_sdcc/platform_view.c
+++ platform_sdcc/platform_view.c
@@ -6,6 +6,7 @@
 #define draw_one_byte() screen_write(pos_local, value); ++pos_local
 #define draw_one_line_of_two_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
 #define draw_one_line_of_four_bytes() pos_local = pos_this_sel; draw_one_byte(); draw_one_byte(); draw_one_byte(); draw_one_byte(); pos_this_sel += line_stride;
+#define grid_origin(row, col, bytes_per_cell_width, chars_height_per_cell_height) screen + row * char_stride * chars_height_per_cell_height + col * bytes_per_cell_width
 
 const uint8_t state2byte[CF_STATECOUNT] = { 0x0C, 0xCC, 0x30, 0xF0, 0x3C, 0xFC };
 typedef void cell_draw_function( uint8_t row, uint8_t col, cf_cellState_t const state );
@@ -15,7 +16,7 @@ void cf_cell_draw_12( uint8_t row, uint8_t col, cf_cellState_t const state )
         uint8_t value = state2byte[state];
         {
                 static const int bytes_per_cell_width = 4;
-                uint8_t *pos_global = screen + row * char_stride * 2 + col * bytes_per_cell_width;
+                uint8_t *pos_global = grid_origin(row, col, bytes_per_cell_width, 2);
                 uint8_t *pos_this_sel = pos_global;
                 uint8_t *pos_local;
                 draw_one_line_of_four_bytes();
@@ -43,7 +44,7 @@ void cf_cell_draw_24( uint8_t row, uint8_t col, cf_cellState_t const state )
         uint8_t value = state2byte[state];
         {
                 static const int bytes_per_cell_width = 2;
-                uint8_t *pos_this_sel = screen + row * char_stride + col * bytes_per_cell_width;
+                uint8_t *pos_this_sel = grid_origin(row, col, bytes_per_cell_width, 1);
                 uint8_t *pos_local;
                 draw_one_line_of_two_bytes();
                 draw_one_line_of_two_bytes();
