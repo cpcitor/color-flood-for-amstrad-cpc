1b40745e58fbb756fc86623ce3ba5972be0c3715
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2014-12-30 20:16:44 +0100

    Declare cf_grid_draw() cf_grid_dump().

--- platform_native/platform_view.h
+++ platform_native/platform_view.h
@@ -1,7 +1,9 @@
 #ifndef __PLATFORM_VIEW_H__
 #define __PLATFORM_VIEW_H__
 
-void cf_model_dump( cf_model_t *this_model );
-void cf_model_draw( cf_model_t *this_model );
+void cf_model_draw( const cf_model_t *const this_model );
+void cf_model_dump( const cf_model_t *const this_model );
+void cf_grid_draw( const cf_grid_t *const this_grid );
+void cf_grid_dump( const cf_grid_t *const this_grid );
 
 #endif /* __PLATFORM_VIEW_H__ */
