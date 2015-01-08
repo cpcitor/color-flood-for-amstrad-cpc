cdc38b8088a2a8f02000447464e83633bc802de1
Stéphane Gourichon <stephane@n55sf-l.gourichon.org>
2015-01-08 12:22:30 +0100

    Colors not get set in C. Program can be run without the basic loader (it will reset on exit).

--- platform_sdcc/cf.bas
+++ platform_sdcc/cf.bas
@@ -1,7 +1,4 @@
 10 mode 0 : border 0 : ink 0,0 : ink 1,26
-16 ink 2,16 : ink 3,25 : ink 4,22 : ink 5,23 : ink 6,14 : ink 7,17
-17 ink 8,15 : ink 9,13
-18 ink 10,3 : ink 11,12 : ink 12,9 : ink 13,10 : ink 14,1 : ink 15,4
 19 locate 1,12
 20 print "    Color  Flood"
 25 openout "heyjude"
--- platform_sdcc/platform.c
+++ platform_sdcc/platform.c
@@ -5,10 +5,39 @@
 
 #include <cfwi/cfwi.h>
 
+enum { ink_count = 16 };
+const uint8_t const inks[ink_count] =
+{
+        0, 26,
+#if CF_PASTEL_COLORS
+        16, 25, 22, 23, 14, 17,
+#else /* CF_PASTEL_COLORS */
+        6, 24, 18, 20, 2, 8,
+#endif /* CF_PASTEL_COLORS */
+        15, 13,
+        3, 12, 9, 10, 1, 4
+};
+
 void cf_platform_init()
 {
+        fw_scr_initialise();
+        fw_scr_set_mode( 0 );
+
         cf_screen_line_to_ptr_init();
 
+        {
+                uint8_t i;
+
+                fw_scr_set_ink( 0xa, 0xb, 0xc );
+
+                for ( i = 0; i < ink_count; i++ )
+                {
+                        uint8_t color = inks[i];
+                        fw_scr_set_ink( i, color, color );
+                }
+
+                fw_scr_set_border( 0, 0 );
+        }
 
         {
                 long unsigned int cpctime = fw_kl_time_please();
