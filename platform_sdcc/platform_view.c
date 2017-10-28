fc242d56ea760c484ba004de2288aee92054f2ad
Stéphane Gourichon <stephane.gourichon@fidergo.fr>
2017-10-28 11:22:07 +0200

    Make variable const.

--- platform_sdcc/platform_view.c
+++ platform_sdcc/platform_view.c
@@ -178,8 +178,8 @@ void cf_player_area_bars( const cf_model_t *const this_model )
 
         while ( y-- != 0 ) // small hack, not really necessary, for shorter ASM
         {
-                uint8_t iplayer = ( y * this_model->playerCount ) >> total_height_exponent;
-                uint16_t area = this_model->domainAreas[iplayer];
+                const uint8_t iplayer = ( y * this_model->playerCount ) >> total_height_exponent;
+                const uint16_t area = this_model->domainAreas[iplayer];
                 const cf_cellState_t player_state = cf_model_get_current_player_state( this_model, iplayer );
                 const uint8_t color = state_to_color( player_state );
                 fw_gra_set_pen( color );
