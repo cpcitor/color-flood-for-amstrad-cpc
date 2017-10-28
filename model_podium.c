e0f8b23c6b1ceede8b70ef69de8b8ba5efc117f5
Stéphane Gourichon <stephane.gourichon@fidergo.fr>
2017-10-28 14:12:59 +0200

    Functions to compute podium.

---
+++ model_podium.c
@@ -0,0 +1,94 @@
+#include "model_podium.h"
+#include "log.h"
+#include <string.h>
+
+
+void cf_model_podium_init( cf_podium_t *this_podium )
+{
+        uint8_t irow;
+
+        memset( this_podium, CF_PODIUM_EMPTY_SEATS_VALUE, sizeof( cf_podium_t ) );
+
+        for ( irow = 0; irow < CF_PODIUM_ROW_COUNT; irow++ )
+        {
+                this_podium->row[irow].area = 0;
+        }
+}
+
+cf_podium_row_t *cf_model_podium_insert_row( cf_podium_t *const this_podium, uint16_t area, uint8_t irow )
+{
+        const uint8_t const number_of_rows_to_copy = CF_PODIUM_ROW_COUNT - irow - 1;
+        cf_podium_row_t *const where_new_row_will_be = &( this_podium->row[irow] );
+
+        if ( number_of_rows_to_copy > 0 )
+        {
+                cf_podium_row_t *const where_shifted_row_goes = where_new_row_will_be + 1;
+                memcpy( where_shifted_row_goes, where_new_row_will_be, sizeof( cf_podium_row_t ) );
+        }
+
+        {
+                cf_podium_row_t *const new_row = where_new_row_will_be;
+                // The memset() below also overwrites the area field,
+                // which is okay, we write it just after.
+                memset( new_row, CF_PODIUM_EMPTY_SEATS_VALUE, sizeof( cf_podium_row_t ) );
+                new_row->area = area;
+                return new_row;
+        }
+}
+
+void cf_model_podium_add_player_at_row( cf_podium_row_t *const this_row, const cf_player_i iplayer )
+{
+        cf_player_i *seat = &( this_row->seat[0] );
+
+        uint8_t iseat = 0;
+
+        for ( iseat = 0; iseat < CF_PODIUM_SEATS_PER_ROW; iseat++ )
+        {
+                if ( ( *seat ) == CF_PODIUM_EMPTY_SEATS_VALUE )
+                {
+                        ( *seat ) = iplayer;
+                        return;
+                }
+
+                ++seat;
+        }
+
+        dbglog_unconditional( "Podium row overflow." );
+}
+
+void cf_model_podium_compute( cf_podium_t *const this_podium, const cf_model_t *const this_model )
+{
+//        cf_model_podium_init();
+
+        cf_player_i iplayer;
+
+        for ( iplayer = 0; iplayer < this_model->playerCount; iplayer++ )
+        {
+                uint16_t this_player_area = this_model->domainAreas[iplayer];
+
+                /** First row we're strictly above -> insert new row. */
+                /** First row we're equal -> add to row. */
+                int irow;
+
+                for ( irow = 0; irow < CF_PODIUM_ROW_COUNT; irow++ )
+                {
+                        cf_podium_row_t *row = &this_podium->row[irow];
+                        uint16_t this_row_area = row->area;
+
+                        if ( this_player_area < this_row_area )
+                        {
+                                continue;
+                        }
+
+                        if ( this_player_area > this_row_area )
+                        {
+                                cf_model_podium_insert_row( this_podium, this_player_area, irow );
+                                // Actually, local variable row already holds the return value of cf_model_podium_insert_row.
+                        }
+
+                        // Whether we reach an existing row, or inserted one, we add our player here.
+                        cf_model_podium_add_player_at_row( row, iplayer );
+                        break; // next player
+                }
+        }
+}
---
+++ model_podium.h
@@ -0,0 +1,39 @@
+#ifndef __MODEL_PODIUM_H__
+#define __MODEL_PODIUM_H__
+
+#include "platform.h"
+#include "model.h"
+
+#define CF_PODIUM_ROW_COUNT CF_MAXPLAYERCOUNT
+#define CF_PODIUM_SEATS_PER_ROW CF_MAXPLAYERCOUNT
+#define CF_PODIUM_EMPTY_SEATS_VALUE CF_MAXPLAYERCOUNT
+
+/**
+
+An empty podium seat holds value CF_MAXPLAYERCOUNT.
+
+*/
+typedef struct
+{
+        uint16_t area;
+        cf_player_i seat[CF_PODIUM_SEATS_PER_ROW];
+} cf_podium_row_t;
+
+
+/** At game end, we compute podium rows, to account for all possible
+* draw-game cases.
+
+* Although technically, only the first row of the podium may hold all
+* players, the second may hold at most all-minus-one players, the
+* third all-minus-two players and so on, we take a simple model with a
+* podium where each row has enough room for all players.
+*/
+typedef struct
+{
+        cf_podium_row_t row[CF_PODIUM_ROW_COUNT];
+} cf_podium_t;
+
+void cf_model_podium_init( cf_podium_t *this_podium );
+void cf_model_podium_compute( cf_podium_t *const this_podium, const cf_model_t *const this_model );
+
+#endif /* __MODEL_PODIUM_H__ */
