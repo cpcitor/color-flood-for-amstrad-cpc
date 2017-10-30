9694b11be76b228d1531e0668f9f9910ad87231c
Stéphane Gourichon <stephane.gourichon@fidergo.fr>
2017-10-30 23:24:24 +0100

    Fix enormous bug in pr_int().

--- print.c
+++ print.c
@@ -20,5 +20,22 @@ void pr_int( int n )
                 n = -n;
         }
 
-        platform_print_char_safe( ( unsigned int ) n );
+        pr_uint( ( unsigned int ) n );
 }
+
+/*
+
+        {
+                int i;
+
+                for ( i = 0; i <= 255; i++ )
+                {
+                        pr_uint( i );
+
+                        if ( ( i & 0x0f ) == 0 )
+                        {
+                                cf_pause();
+                        }
+                }
+        }
+*/
