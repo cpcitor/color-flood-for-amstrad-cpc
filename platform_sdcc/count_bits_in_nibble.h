6e8e5c9ae9c1ab40351be1b63222db985c1b373f
Stéphane Gourichon <stephane.gourichon@fidergo.fr>
2017-10-31 07:07:38 +0100

    Minor cleanup.

--- platform_sdcc/count_bits_in_nibble.h
+++ platform_sdcc/count_bits_in_nibble.h
@@ -1,6 +1,8 @@
 #ifndef __COUNT_BITS_IN_NIBBLE_H__
 #define __COUNT_BITS_IN_NIBBLE_H__
 
+#include <stdint.h>
+
 uint8_t count_bits_in_nibble( uint8_t value );
 
 #endif /* __COUNT_BITS_IN_NIBBLE_H__ */
