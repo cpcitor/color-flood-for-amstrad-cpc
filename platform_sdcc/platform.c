f706e4799e31ce59eba6b834d414aaa6f4e1071f
cpcitor <stephane_cpcitor@gourichon.org>
2017-12-28 20:00:29 +0100

    Revert "Use one call to MC SET INKS instead of a loop to SCR SET INK ONE. Save 32 bytes."
    This reverts commit 771d2bb0cd27a3601f53ccc9b45bb570412d19cf.

--- platform_sdcc/platform.c
+++ platform_sdcc/platform.c
@@ -7,37 +7,34 @@
 
 #include "platform_view.h"
 
-static const ink_vector16 game_palette =
+enum { ink_count = 16 };
+static const uint8_t const inks[ink_count] =
 {
-        {
-                hardware_color_r0_g0_b0_black,
-                hardware_color_r0_g0_b0_black,
-                hardware_color_r2_g2_b2_bright_white,
-                hardware_color_r2_g0_b0_bright_red,
-                hardware_color_r2_g1_b0_orange,
-                //rgb_to_cpc_color( 2, 2, 0 ), yellow is too close to green
-                hardware_color_r0_g2_b0_bright_green, // not 0,2,1 too bright
-                hardware_color_r0_g2_b2_bright_cyan,
-                hardware_color_r0_g0_b2_bright_blue, // not 0,1,2 which would be too similar to 1,0,2
-                //rgb_to_cpc_color( 1, 0, 2 ), too similar to 0,0,2 on a real CPC
+        0, 26,
+        rgb_to_cpc_color( 2, 0, 0 ),
+        rgb_to_cpc_color( 2, 1, 0 ),
+        //rgb_to_cpc_color( 2, 2, 0 ), yellow is too close to green
+        rgb_to_cpc_color( 0, 2, 0 ), // not 0,2,1 too bright
+        rgb_to_cpc_color( 0, 2, 2 ),
+        rgb_to_cpc_color( 0, 0, 2 ), // not 0,1,2 which would be too similar to 1,0,2
+        //rgb_to_cpc_color( 1, 0, 2 ), too similar to 0,0,2 on a real CPC
 
-                // rgb_to_cpc_color( 2, 0, 1 ), too aggressive
-                //rgb_to_cpc_color( 1, 2, 0 ), too similar to 0,2,1
+        // rgb_to_cpc_color( 2, 0, 1 ), too aggressive
+        //rgb_to_cpc_color( 1, 2, 0 ), too similar to 0,2,1
 
-                hardware_color_r2_g0_b2_bright_magenta,
+        rgb_to_cpc_color( 2, 0, 2 ),
 
-//      hardware_color_r1, 0, 1 ), too similar to 1,0,2
-                hardware_color_r0_g1_b1_cyan,
-                hardware_color_r1_g1_b0_yellow,
-                hardware_color_r1_g0_b1_magenta
-                /* #if CF_PASTEL_COLORS */
-                /*         16, 25, 22, 23, 14, 17, */
-                /* #else /\* CF_PASTEL_COLORS *\/ */
-                /*         6, 24, 18, 20, 2, 8, */
-                /* #endif /\* CF_PASTEL_COLORS *\/ */
-                /*        15, 13,
-                          3, 12, 9, 10, 1, 4*/
-        }
+//        rgb_to_cpc_color( 1, 0, 1 ), too similar to 1,0,2
+        rgb_to_cpc_color( 0, 1, 1 ),
+        rgb_to_cpc_color( 1, 1, 0 ),
+        rgb_to_cpc_color( 1, 0, 1 ),
+        /* #if CF_PASTEL_COLORS */
+        /*         16, 25, 22, 23, 14, 17, */
+        /* #else /\* CF_PASTEL_COLORS *\/ */
+        /*         6, 24, 18, 20, 2, 8, */
+        /* #endif /\* CF_PASTEL_COLORS *\/ */
+        /*        15, 13,
+                  3, 12, 9, 10, 1, 4*/
 };
 
 void cf_platform_init()
@@ -64,7 +61,18 @@ void cf_platform_prepare_for_game_board()
         fw_txt_initialise();
         cf_screen_line_to_ptr_init();
 
-        fw_mc_set_inks( &game_palette );
+        {
+                uint8_t i;
+
+                for ( i = 0; i < ink_count; i++ )
+                {
+                        uint8_t color = inks[i];
+                        fw_scr_set_ink_one( i, color );
+                }
+
+                fw_scr_set_border( 0, 0 );
+        }
+
 
         {
                 //enum { char_matrix_definition_char_size }
