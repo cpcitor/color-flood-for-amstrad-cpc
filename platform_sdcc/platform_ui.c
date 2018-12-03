18a1ee9c50d23b046ff45db19c2f235aa5a79416
Stéphane Gourichon (cpcitor) <stephane_cpcitor@gourichon.org>
2018-12-03 13:28:45 +0100

    Refine cppcheck checks.

--- platform_sdcc/platform_ui.c
+++ platform_sdcc/platform_ui.c
@@ -2,14 +2,18 @@
 
 #include "platform.h"
 
+#ifdef DEBUG
+
 void cf_pause()
 {
         cfwi_txt_str0_output( "?" );
         fw_km_wait_key();
 }
 
-/* void cf_pause_message( char *msg ) */
-/* { */
-/*         cfwi_txt_str0_output( msg ); */
-/*         fw_km_wait_key(); */
-/* } */
+void cf_pause_message( char *msg )
+{
+        cfwi_txt_str0_output( msg );
+        fw_km_wait_key();
+}
+
+#endif
